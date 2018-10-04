ee664ca4e0a2fba3cbf96d968043af2c92e3d6f5
Andy Kukuc <jeep98.ak@gmail.com>
2018-10-03 19:25:24 -0500

    fixed the spacing

--- JScript/script.js
+++ JScript/script.js
@@ -4,7 +4,7 @@ Project 1 JavaScript
 9-12-2018*/
 
 function myFunction() {
-	alert("Thank You For Submitting!");
-	window.open("https://thewitcher.com/en/witcher3");
-		return false;
+  alert("Thank You For Submitting!");
+  window.open("https://thewitcher.com/en/witcher3");
+  return false;
 }
